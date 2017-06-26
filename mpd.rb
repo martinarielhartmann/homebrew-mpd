4c1f5c1fd47bfba9a6cb5732d47114a2378a4bbf
martinarielhartmann <martinarielhartmann@gmail.com>
2017-06-26 20:31:25 +0300

    Update mpd.rb

--- mpd.rb
+++ mpd.rb
@@ -11,7 +11,7 @@ class Mpd < Formula
   end
 
   head do
-    url "git://yousource.it.jyu.fi/npng/mpd-win32.git"
+    url "git://yousource.it.jyu.fi/~maarhart/npng/mpd.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
